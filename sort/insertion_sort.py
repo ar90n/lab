5ab1e8de534395b42ec90c9dd24c67d062afc0b6
Masahiro Wada <argon.argon.argon@gmail.com>
2014-10-02 08:38:20 +0900

    modify bubble sort and add insertion_sort and selection sort

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-
 import random
 
-def bubble_sort( data ):
+def insertion_sort( data ):
     length = len( data )
 
     for i in range( 1, length ):
-        for j in range( length - i ):
-            if data[ j + 1 ] < data[ j ] :
-                tmp = data[ j + 1 ]
-                data[ j + 1 ] = data[ j ]
-                data[ j ] = tmp
-
-                print( data )
-
+        for j in range( i, 0, -1 ):
+            if data[ j - 1] < data[ j ]:
+                break;
+            tmp = data[ j ]
+            data[ j ] = data[ j - 1 ]
+            data[ j - 1 ] = tmp
+        print( data )
+    pass
 
 def main():
     org_data = [ random.randint( 0, 10000 ) for i in range( 20 ) ]
     print( org_data )
-    bubble_sort( org_data )
+    insertion_sort( org_data )
     print( org_data )
     return
 

d9779690f2cfedfbd36b734b8c6ed5a04170813b
Masahiro Wada <argon.argon.argon@gmail.com>
2014-11-21 07:25:08 +0900

    complete graphcut noise reduction

--- image_processing/graphcut_nr.py
+++ image_processing/graphcut_nr.py
@@ -7,29 +7,80 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 from future_builtins import *
 
+import itertools
+import copy
+
 import cv2
 import numpy as np
 import networkx as nx
 
-def idx( x, y, i, w, h ):
-    return i * w * h + y * w + x
+def create_image():
+    img = np.zeros( ( 64, 64 ))
+    img[16:48, 16:48] = 255
+    img[24:40, 24:40] = 0
+    noise = np.random.rand(64,64)
+    img[ noise < 0.1 ] = 0
+    img[ 0.9 < noise ] = 255
+    return img
+
+def point_to_index( i, x, y, w, h ):
+    return 2 + ( i * w * h + y * w + x )
+
+def index_to_point( index, w, h ):
+    i = ( ( index - 2 ) // ( w * h ) )
+    y = ( ( index - 2 ) %  ( w * h ) ) // w
+    x = ( ( index - 2 ) %  ( w * h ) ) %  w
+    return ( i, x, y )
 
 def main( ):
+    lamb = 40
+    kappa = 9 * 255
+    infinity = 100000
+
+    img = create_image()
+    w = img.shape[0]
+    h = img.shape[1]
+
     g = nx.DiGraph()
+    g.add_nodes_from( range( w * h * 1 + 2 ) )
+    for x,y in itertools.product( range( w ), range( h ) ):
+        cost = abs( img[y,x] - 0 ) * lamb
+        index = point_to_index( 0, x, y, w, h )
+        g.add_edge( 0, index, { 'capacity' : cost } )
 
-    g.add_node( 0 )
-    g.add_node( 1 )
-    g.add_node( 2 )
-    g.add_node( 3 )
+        cost = abs( img[y,x] - 255 ) * lamb
+        index = point_to_index( 0, x, y, w, h )
+        g.add_edge( index, 1, { 'capacity' : cost } )
 
-    g.add_edge( 0, 1, { "capacity" : 8 } )
-    g.add_edge( 0, 2, { "capacity" : 2 } )
-    g.add_edge( 1, 2, { "capacity" : 1 } )
-    g.add_edge( 1, 3, { "capacity" : 3 } )
-    g.add_edge( 2, 3, { "capacity" : 7 } )
+    dxs = [ 1, -1, 0, 1 ]
+    dys = [ 0,  1, 1, 1 ]
+    for i,x,y in itertools.product( range( 1 ), range( w ), range( h ) ):
+        index = point_to_index( i, x, y, w, h )
+        for dx,dy in zip( dxs, dys ):
+            neighbor_x = x + dx
+            neighbor_y = y + dy
+            if 0 <= neighbor_x and neighbor_x < w and 0 <= neighbor_y and neighbor_y < h:
+                neighbor_index = point_to_index( i, neighbor_x, neighbor_y, w, h )
+                g.add_edge( index, neighbor_index, { 'capacity' : kappa } )
+                g.add_edge( neighbor_index, index, { 'capacity' : kappa } )
 
-    print( nx.maximum_flow( g, 0, 3 ) )
-    print( nx.minimum_cut( g, 0, 3 ) )
+    cut_value, partition = nx.minimum_cut( g, 0, 1 )
+    reachable, non_reachable = partition
+
+    cutset = set()
+    for u,nbrs in ( (n, g[n]) for n in reachable ):
+        cutset.update( (u,v) for v in nbrs if v in non_reachable )
+
+    res = copy.deepcopy( img )
+    for ind1,ind2 in cutset:
+        if ind1 <= 1 or ind2 <= 1:
+            i,x,y = index_to_point( max(ind1,ind2), w, h )
+            res[y,x] = 255 * min(ind1,ind2)
+            pass
+
+    cv2.imshow('res', res)
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
     return
 
 

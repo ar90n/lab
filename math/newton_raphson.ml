42358eb1eea6f9eebbf886aaf65c0dfa7a109c15
Masahiro Wada <argon.argon.argon@gmail.com>
2015-01-15 08:30:33 +0000

    add ocaml studies 2

---
+++ math/newton_raphson.ml
@@ -0,0 +1,17 @@
+let deriv f =
+    let dx = 0.1e-10 in
+        fun x -> ( f( x +. dx ) -. f( x ) ) /. dx;;
+
+let fixpoint f init =
+    let threshold  = 0.1e-10 in
+    let rec loop x = 
+        let next = f x in
+        if abs_float( x -. next ) < threshold then x
+        else loop next
+    in loop init;;
+
+let newton_transform f = fun x -> x -. f(x) /. ( deriv f x );;
+let newton_method f guess = fixpoint ( newton_transform f ) guess;;
+let square_root a = newton_method ( fun x -> x *. x -. a ) 1.0;;
+
+print_endline ( string_of_float ( square_root 4.0 ) );;

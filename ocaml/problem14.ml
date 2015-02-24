7bfe228375b2e79c74bf6c17e753c2a990ae7d2c
Masahiro Wada <argon.argon.argon@gmail.com>
2015-02-24 15:53:41 +0000

    add some problems

---
+++ ocaml/problem14.ml
@@ -0,0 +1,7 @@
+let rec replicate l n =
+let rec ext c n = if n = 0 then [] else c :: ext c ( n - 1 ) in
+match l with
+| [] as l -> l
+| h :: r -> ( ext h n ) @ replicate r n;;
+
+replicate ["a";"b";"c"] 3;;

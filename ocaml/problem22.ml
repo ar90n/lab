2d59bd4e24f1ffc658db61c49c49c9bfe24e9157
Masahiro Wada <argon.argon.argon@gmail.com>
2015-02-25 16:34:22 +0000

    change physical equality to structural equality

--- ocaml/problem22.ml
+++ ocaml/problem22.ml
@@ -1,6 +1,6 @@
 let range b e =
 let step = if b < e then 1 else -1 in
-let rec aux acc b e = if b != e then aux (b::acc) (b+step) e else b::acc in
+let rec aux acc b e = if b <> e then aux (b::acc) (b+step) e else b::acc in
 List.rev ( aux [] b e );;
 
 range 4 9;;

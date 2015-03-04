83d78b66eaa2347f402b15a85022164c821cabe9
Masahiro Wada <argon.argon.argon@gmail.com>
2015-03-04 10:11:25 +0000

    add problem41.ml

--- ocaml/problem41.ml
+++ ocaml/problem41.ml
@@ -4,7 +4,7 @@ type bool_expr =
   |And of bool_expr * bool_expr
   |Or of bool_expr * bool_expr;;
 
-let table2 v1 v2 expr = 
+let table vs expr = 
   let rec look_up_var var_name = function
   | [] -> raise Not_found
   | [(name,v)] -> if name = var_name then v else raise Not_found
@@ -14,7 +14,14 @@ let table2 v1 v2 expr =
   | Not( expr ) -> not ( aux var_tab expr )
   | And( lh, rh ) -> ( aux var_tab lh ) && ( aux var_tab rh )
   | Or ( lh, rh ) -> ( aux var_tab lh ) || ( aux var_tab rh ) in
-  let var_tabs = [[( v1, true ); (v2, true )]; [(v1,true);(v2,false)];[(v1,false);(v2,true)];[(v1,false);(v2,false)]] in
-  List.map (fun tab -> let (_,v1_val)::(_,v2_val)::t = tab in ( v1_val, v2_val, aux tab expr ) ) var_tabs;;
+let gen_tab l = 
+let rec aux acc = function
+| [] -> [acc]
+| h::t -> ( aux ((h,true)::acc) t ) @ ( aux ((h,false)::acc) t ) in
+aux [] l in
+List.map (fun tab -> ( tab, aux tab expr ) ) ( gen_tab vs );;
 
-table2 "a" "b" (And(Var "a", Or(Var "a", Var "b")));;
+table ["a";"b"] (And(Var "a", Or(Var "a", Var "b")));;
+
+let a = Var "a" and b = Var "b" and c = Var "c" in
+table ["a"; "b"; "c"] (Or(And(a, Or(b,c)), Or(And(a,b), And(a,c))));;

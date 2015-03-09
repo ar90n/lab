2316c37b436875e7d8aaa0827bab76f85a23275c
Masahiro Wada <argon.argon.argon@gmail.com>
2015-03-09 00:28:28 +0000

    add problem44.ml

---
+++ ocaml/problem44.ml
@@ -0,0 +1,14 @@
+type 'a binary_tree =
+| Empty
+| Node of 'a * 'a binary_tree * 'a binary_tree;;
+
+let rec cbal_tree = function
+| 0 ->[Empty]
+| n ->
+let n = n - 1 in
+let n1 = n / 2 in
+let n2 = n - n1 in
+let merge_tree l r all = List.fold_left ( fun acc x -> ( List.fold_left ( fun a y -> Node('x', x, y)::a ) acc r ) ) all l in
+if n1 = n2 then (let h = cbal_tree n1 in merge_tree h h []) else( let lh = cbal_tree n1 in let rh = cbal_tree n2 in  merge_tree lh rh ( merge_tree rh lh [] ));;
+
+List.length( cbal_tree 40);;

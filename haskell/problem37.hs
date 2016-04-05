4e7d45b24b43961594345a59946087f3a82e1bcf
Masahiro Wada <argon.argon.argon@gmail.com>
2016-04-05 10:45:05 +0900

    add 37 and trivial fix

--- haskell/problem37.hs
+++ haskell/problem37.hs
@@ -6,3 +6,5 @@ primeFactorsMult xs = map (\a -> (head a, length a)) . group $ aux 2 xs
                             aux p x = if x `mod` p == 0 then p : (aux p (x `div` p))
                                                         else (aux (p+1) x)
 
+totient:: Int -> Int
+totient m = product [(p - 1) * p ^ (c - 1) | (p, c) <- primeFactorsMult m]

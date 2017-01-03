0d967ec156dd790f5937a41e3ebce1689706cad2
Masahiro Wada <argon.argon.argon@gmail.com>
2017-01-03 13:34:45 +0900

    first commit

---
+++ components/SiteNav/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { RouteHandler, Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import './style.css'
+
+class SiteNav extends React.Component {
+    render() {
+        const {location} = this.props
+        return (
+            <nav className='blog-nav'>
+              <ul>
+                <li>
+                  <Link to={ prefixLink('/')} activeClassName="current" onlyActiveOnIndex> Articles
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            );
+    }
+}
+
+SiteNav.propTypes = {
+    location: React.PropTypes.object,
+}
+
+export default SiteNav

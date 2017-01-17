9d5193f0c4be9a6692f7c3a41a34cf698249ff98
Masahiro Wada <argon.argon.argon@gmail.com>
2017-01-18 00:47:13 +0900

    add ga

--- gh-pages/html.js
+++ gh-pages/html.js
@@ -14,8 +14,18 @@ module.exports = React.createClass({
         const {title} = Helmet.rewind()
         const font = <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,700&subset=latin,cyrillic' rel='stylesheet' type='text/css' />
         let css
+        let ga
         if (process.env.NODE_ENV === 'production') {
             css = <style dangerouslySetInnerHTML={ {    __html: require('!raw!./public/styles.css')} } />
+            ga = <script dangerouslySetInnerHTML={ { __html: `
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+              ga('create', 'UA-90473091-1', 'auto');
+              ga('send', 'pageview');
+            ` } } />
         }
 
         return (
@@ -31,8 +41,11 @@ module.exports = React.createClass({
             <body>
               <div id="react-mount" dangerouslySetInnerHTML={ {    __html: this.props.body} } />
               <script src={ prefixLink(`/bundle.js?t=${BUILD_TIME}`) } />
+              { ga }
             </body>
             </html>
         )
     },
 })
+/*
+*/

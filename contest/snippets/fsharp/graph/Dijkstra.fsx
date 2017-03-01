82f37ecf615fc250382871a0e2d86fe25ce85fbf
Masahiro Wada <argon.argon.argon@gmail.com>
2017-03-01 16:12:45 +0900

    update dijkstra

--- contest/snippets/fsharp/graph/Dijkstra.fsx
+++ contest/snippets/fsharp/graph/Dijkstra.fsx
@@ -1,20 +1,27 @@
-let Dijkstra (g : graph) (root : vertex) : cost list =
+let Dijkstra (g : graph) (root : vertex) : (graph * cost array) =
     let nodes = Nodes g
     let costs = Array.create nodes System.Double.PositiveInfinity
+
     let comp (_,a) (_,b) = 
         match a,b with
         | _ when a < b -> -1
         | _ when a > b ->  1
         | _ -> 0
-    let pqueue = ref <| PriorityQueue comp
-
-    pqueue := Push !pqueue (root, 0.0)
-    while( not (IsEmpty !pqueue) ) do
-        let ((v,c),npqueue) = Pop !pqueue
-        pqueue := npqueue
-        if  System.Double.IsInfinity costs.[v] then
-            costs.[v] <- c
-            Seq.zip g.[v].Keys g.[v].Values |> Seq.iter ( fun (nv,ec) -> pqueue := (Push !pqueue (nv,c + ec)) )
-    done
-
-    Array.toList costs
+    let pqueue = Edges g |> List.fold (fun q (f,t,c) -> Push q ((f,t,c),c)) (PriorityQueue comp)
+    costs.[root] <- 0.0
+    let rec doit (costs : cost array) (edges : edge list) (pqueue : (edge * cost) pqueue) : cost array * edge list =
+        if IsEmpty pqueue then
+            costs, edges
+        else
+            let (((_,t,c) as e,ac),pqueue') = Pop pqueue
+            if ac < costs.[t] then
+                let edges' = e :: edges
+                costs.[t] <- ac
+                Seq.zip g.[t].Keys g.[t].Values
+                |> Seq.map (fun (t',c') -> (t,t',c'))
+                |> Seq.fold ( fun q (f,t,c) -> Push q ((f,t,c), ac + c) ) pqueue'
+                |> doit costs edges'
+            else
+                doit costs edges pqueue'
+    let (rc,re ) = doit costs [] pqueue
+    DGraph nodes re, rc

33e416a66730492ab10c7a06ad2c492c1f16668f
Masahiro Wada <argon.argon.argon@gmail.com>
2017-03-13 13:21:20 +0900

    updat generator script

--- contest/snippets/generate_snippets.py
+++ contest/snippets/generate_snippets.py
@@ -2,6 +2,7 @@
 import glob
 import os
 import sys
+import functools
 
 snippet_prefix = "____"
 
@@ -15,19 +16,57 @@ suffix_map = {
     "fsharp" : ".fsx"
 }
 
+comment_prefix_map = {
+    "haskell" : "//",
+    "cpp" : "//",
+    "fsharp" : "//"
+}
+
+def remove_comment( str_array, lang ):
+    return filter( lambda l: not l.startswith(comment_prefix_map[lang]), str_array)
+
+def get_deps( str_array, lang ):
+    return map( lambda l : (l[2:-1]).rstrip().lstrip(), filter( lambda l: l.startswith(comment_prefix_map[lang]), str_array))
+
+def toposort( dep ):
+    visited = dict([ (k,0) for k in dep.keys() ])
+    def doit( root ):
+        res = []
+        if visited[root] == 0:
+            visited[root] = 1
+            for k2 in dep[root]:
+                res = res + doit( k2 )
+            res.append(root)
+            visited[root] = 2
+        elif visited[root] == 1:
+            raise "has loop"
+
+        return res
+
+    res = []
+    for k in dep.keys():
+        if visited[k] == 0:
+            res = res + doit( k )
+
+    return res
+
 def convert_module( lang, module_name ):
     module_dir = os.path.join('.', module_name)
     func_pattern = '*' + suffix_map[lang]
     funcs = [func for func in glob.glob(os.path.join(module_dir,func_pattern))]
 
-    res = []
+    res = {}
+    dep = {}
     for func_path in funcs:
         func_name = os.path.basename(func_path).split('.')[0]
-        content = ''.join( open(func_path).readlines() )[:-1]
+        lines = open(func_path).readlines()
+        content = ''.join( remove_comment( lines, lang ) )[:-1]
         snippet_element = snippet_template % ( snippet_prefix, func_name, "", content )
-        res.append( snippet_element )
+        snippet_key = os.path.join(module_name,os.path.basename(func_path))
+        res[snippet_key] = snippet_element
+        dep[snippet_key] = get_deps(lines, lang)
 
-    return '\n\n'.join( res )
+    return (res,dep)
 
 def main():
     langs = [lang for lang in glob.glob('*') if os.path.isdir( lang ) ]
@@ -37,11 +76,16 @@ def main():
         if not lang in suffix_map:
             continue
 
+        dep_map = {}
         lang_dir = os.path.join( root_dir, lang )
         os.chdir( lang_dir )
 
-        modules = [ convert_module( lang, module ) for module in glob.glob('*') if os.path.isdir( module ) and module != 'test']
-        snippets_str = '\n\n'.join( modules )
+        res,dep = functools.reduce( lambda a,b: ({**(a[0]),**(b[0])},{**(a[1]),**(b[1])}), [ convert_module( lang, module ) for module in glob.glob('*') if os.path.isdir( module ) and module != 'test'],({},{}))
+        order = toposort( dep )
+        snippets_strs = []
+        for k in order:
+            snippets_strs.append( res[k] )
+        snippets_str = '\n\n'.join( snippets_strs )
 
         os.chdir( root_dir )
         if 0 < len( snippets_str ):
--- contest/snippets/generate_test.py
+++ contest/snippets/generate_test.py
@@ -2,6 +2,7 @@
 import glob
 import os
 import sys
+import functools
 
 snippet_prefix = "____"
 
@@ -17,14 +18,67 @@ suffix_map = {
     "fsharp" : ".fsx"
 }
 
+comment_prefix_map = {
+    "haskell" : "//",
+    "cpp" : "//",
+    "fsharp" : "//"
+}
+
+def remove_comment( str_array, lang ):
+    return filter( lambda l: not l.startswith(comment_prefix_map[lang]), str_array)
+
+def get_deps( str_array, lang ):
+    return map( lambda l : (l[2:-1]).rstrip().lstrip(), filter( lambda l: l.startswith(comment_prefix_map[lang]), str_array))
+
+def toposort( dep , roots = None ):
+    if roots == None:
+        roots = dep.keys()
+    visited = dict([ (k,0) for k in dep.keys() ])
+    def doit( root ):
+        res = []
+        if visited[root] == 0:
+            visited[root] = 1
+            for k2 in dep[root]:
+                res = res + doit( k2 )
+            res.append(root)
+            visited[root] = 2
+        elif visited[root] == 1:
+            raise "has loop"
+
+        return res
+
+    res = []
+    for k in roots:
+        if visited[k] == 0:
+            res = res + doit( k )
+
+    return res
+
 def convert_module( lang, module_name ):
+    module_dir = os.path.join('.', module_name)
+    func_pattern = '*' + suffix_map[lang]
+    funcs = [func for func in glob.glob(os.path.join(module_dir,func_pattern))]
+
+    res = {}
+    dep = {}
+    for func_path in funcs:
+        func_name = os.path.basename(func_path).split('.')[0]
+        lines = open(func_path).readlines()
+        content = ''.join( remove_comment( lines, lang ) )[:-1]
+        snippet_key = os.path.join(module_name,os.path.basename(func_path))
+        res[snippet_key] = content
+        dep[snippet_key] = get_deps(lines, lang)
+
+    return (res,dep)
+
+def convert_module2( lang, module_name, res, dep ):
     module_dir = os.path.join('.', module_name)
     test_template_path = os.path.join( module_dir, 'test.template' )
     if not os.path.exists( test_template_path ):
         return None
 
     func_pattern = '*' + suffix_map[lang]
-    funcs = [func for func in glob.glob(os.path.join(module_dir,func_pattern))]
+    funcs = [func[2:] for func in glob.glob(os.path.join(module_dir,func_pattern))]
 
     primary_func = os.path.join( module_dir, module_name + suffix_map[lang] )
     try:
@@ -33,12 +87,11 @@ def convert_module( lang, module_name ):
     except:
         pass
 
-    res = []
-    for func_path in funcs:
-        func_name = os.path.basename(func_path).split('.')[0]
-        content = ''.join( open(func_path).readlines() )[:-1]
-        res.append( content )
-    module_str = '\n\n'.join( res )
+    order = toposort( dep, funcs )
+    snippets_strs = []
+    for k in order:
+        snippets_strs.append( res[k] )
+    module_str = '\n\n'.join( snippets_strs )
 
     test_template = ''.join(open(test_template_path).readlines())[:-1]
     return test_template.replace( replace_str, module_str )
@@ -54,11 +107,12 @@ def main():
         lang_dir = os.path.join( root_dir, lang )
         os.chdir( lang_dir )
 
+        res,dep = functools.reduce( lambda a,b: ({**(a[0]),**(b[0])},{**(a[1]),**(b[1])}), [ convert_module( lang, module ) for module in glob.glob('*') if os.path.isdir( module ) and module != 'test'],({},{}))
         for module in glob.glob('*'):
             if not os.path.isdir( module ):
                 continue
 
-            test_str = convert_module( lang, module )
+            test_str = convert_module2( lang, module, res, dep )
             if test_str is None:
                 continue
 

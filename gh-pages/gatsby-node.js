65938d9ac880e8e999f951dfaebf69521c1fc275
Masahiro Wada <argon.argon.argon@gmail.com>
2017-05-23 22:21:58 +0900

    add sitemap

--- gh-pages/gatsby-node.js
+++ gh-pages/gatsby-node.js
@@ -1,8 +1,37 @@
 var rucksack = require('rucksack-css')
 var lost = require("lost")
 var cssnext = require("postcss-cssnext")
+var fs = require('fs-extra-promise') //install this package
+var sm = require('sitemap') // install this package
 
-exports.modifyWebpackConfig = function(config, env) {
+function pagesToSitemap(pages) {
+  var urls = pages.map(function(p) {
+    if (p.path !== undefined) {
+      return {
+        url: p.path,
+        changefreq: 'daily',
+        priority: 0.7
+      }
+    }
+  })
+  // remove undefined (template pages)
+  return urls.filter(function(u) { return u !== undefined})
+}
+
+function generateSiteMap(pages) {
+  var sitemap = sm.createSitemap({
+    hostname: 'http://lab.ar90n.net',
+    cacheTime: '60000',
+    urls: pagesToSitemap(pages),
+  })
+  console.log('Generating sitemap.xml')
+  fs.writeFileSync(
+    `${__dirname}/public/sitemap.xml`,
+    sitemap.toString()
+  )
+}
+
+module.exports.modifyWebpackConfig = function(config, env) {
     config.merge({
         postcss: [
             lost(),
@@ -20,3 +49,8 @@ exports.modifyWebpackConfig = function(config, env) {
 
     return config
 };
+
+module.exports.postBuild = function(pages, callback) {
+  generateSiteMap(pages)
+  callback()
+}

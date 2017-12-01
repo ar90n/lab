b6cb2b8233e94043a15e3d510c71c59bc38f777e
Masahiro Wada <argon.argon.argon@gmail.com>
2017-12-01 09:40:53 +0900

    trivial fix

--- docker_settings/make_images.sh
+++ docker_settings/make_images.sh
@@ -9,12 +9,3 @@ ${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg gr
 ${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/miniconda3 ./miniconda3
 ${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/miniconda2 ./miniconda2
 ${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/tensorflow ./tensorflow
-#${DOCKER_CMD} build --build-arg user_name=$USER_NAME -t ${GCR_REGION}/ar90n_devenv/base ./base
-#${DOCKER_CMD} build --build-arg user_name=$USER_NAME -t ${GCR_REGION}/ar90n_devenv/cxx ./cxx
-#${DOCKER_CMD} build --build-arg user_name=$USER_NAME -t ${GCR_REGION}/ar90n_devenv/js ./js
-#${DOCKER_CMD} build --build-arg user_name=$USER_NAME -t ${GCR_REGION}/ar90n_devenv/haskell ./haskell
-#${DOCKER_CMD} build --build-arg user_name=$USER_NAME -t ${GCR_REGION}/ar90n_devenv/mono ./mono
-#${DOCKER_CMD} build --build-arg user_name=$USER_NAME -t ${GCR_REGION}/ar90n_devenv/alg ./alg
-#${DOCKER_CMD} build --build-arg user_name=$USER_NAME -t ${GCR_REGION}/ar90n_devenv/rust ./rust
-#${DOCKER_CMD} build --build-arg user_name=$USER_NAME -t ${GCR_REGION}/ar90n_devenv/devall ./devall
-#${DOCKER_CMD} build --build-arg user_name=$USER_NAME -t ${GCR_REGION}/ar90n_devenv/madoko ./madoko

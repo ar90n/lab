d59ad36d0185c599492081134a092593e4cdba78
Masahiro Wada <argon.argon.argon@gmail.com>
2017-12-15 00:43:16 +0900

    update docker settings

--- docker_settings/make_images.sh
+++ docker_settings/make_images.sh
@@ -5,7 +5,11 @@ _GROUP=`id -gn ${USER}`
 _GID=`id -g ${USER}`
 DOCKER_CMD=${DOCKER_CMD:-'docker'}
 
-${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/dev_env ./dev_env
-${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/miniconda3 ./miniconda3
-${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/miniconda2 ./miniconda2
-${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/tensorflow ./tensorflow
+#${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/dev_env ./dev_env
+#${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/miniconda3 ./miniconda3
+#${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/miniconda2 ./miniconda2
+#${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/python_all_in_one ./python_all_in_one
+#${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/mono ./mono
+#${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID --build-arg node_version="v9.2.0" -t ar90n/node_v9.2.0 ./node
+${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID  -t ar90n/cxx ./cxx
+#${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID  -t ar90n/rust ./rust

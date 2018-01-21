b724884869f1adc3ddb3539cffd0ca7f4ec8af82
Masahiro Wada <argon.argon.argon@gmail.com>
2018-01-22 00:57:20 +0900

    add docker file for juyter_root_rise

--- docker_settings/make_images.sh
+++ docker_settings/make_images.sh
@@ -8,9 +8,11 @@ DOCKER_CMD=${DOCKER_CMD:-'docker'}
 #${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID -t ar90n/dev_env ./dev_env
 #${DOCKER_CMD} build --build-arg user=$_USER -t ar90n/miniconda3 ./miniconda3
 #${DOCKER_CMD} build --build-arg user=$_USER -t ar90n/miniconda2 ./miniconda2
-#${DOCKER_CMD} build --build-arg user=$_USER -t ar90n/python_all_in_one ./python_all_in_one
+#${DOCKER_CMD} build --build-arg user=$_USER --build-arg group=$_GROUP -t ar90n/python_all_in_one ./python_all_in_one
 #${DOCKER_CMD} build --build-arg user=$_USER -t ar90n/mono ./mono
 #${DOCKER_CMD} build --build-arg user=$_USER --build-arg node_version="v9.2.0" -t ar90n/node_v9.2.0 ./node
 #${DOCKER_CMD} build --build-arg user=$_USER -t ar90n/cxx ./cxx
-${DOCKER_CMD} build --build-arg user=$_USER -t ar90n/cxx_improc ./cxx_improc
+#${DOCKER_CMD} build --build-arg user=$_USER -t ar90n/cxx_improc ./cxx_improc
 #${DOCKER_CMD} build --build-arg user=$_USER -t ar90n/rust ./rust
+
+${DOCKER_CMD} build --build-arg user=$_USER --build-arg uid=$_UID --build-arg group=$_GROUP --build-arg gid=$_GID --build-arg port=8008 -t ar90n/jupyter_root_rise ./jupyter_root_rise

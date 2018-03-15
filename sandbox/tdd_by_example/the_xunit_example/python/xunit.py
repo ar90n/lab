cce8b3130f09c037a7d5f4b542a9e9e9da798eaa
Masahiro Wada <argon.argon.argon@gmail.com>
2018-03-16 03:14:41 +0900

    chapter20

--- sandbox/tdd_by_example/the_xunit_example/python/xunit.py
+++ sandbox/tdd_by_example/the_xunit_example/python/xunit.py
@@ -9,39 +9,40 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 
 class WasRun(TestCase):
 
     def setUp(self):
         self.wasRun = None
-        self.wasSetUp = 1
+        self.log = 'setUp '
 
     def testMethod(self):
         self.wasRun = 1
+        self.log += 'testMethod '
+
+    def tearDown(self):
+        self.log += 'tearDown '
 
 
 class TestCaseTest(TestCase):
 
-    def setUp(self):
-        self.test = WasRun('testMethod')
-
-    def testRunning(self):
-        self.test.run()
-        assert self.test.wasRun
-
-    def testSetUp(self):
-        self.test.run()
-        assert self.test.wasSetUp
+    def testTemplateMethod(self):
+        test = WasRun('testMethod')
+        test.run()
+        assert test.log == 'setUp testMethod tearDown '
 
 
 def main():
-    TestCaseTest('testRunning').run()
-    TestCaseTest('testSetUp').run()
+    TestCaseTest('testTemplateMethod').run()
 
 if __name__ == '__main__':
     main()

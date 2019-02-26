acb8e4f0ee387d57356e4511fce0f4a67c1d6995
ar90n <argon.argon.argon@gmail.com>
2019-02-26 08:35:52 +0000

    update singularity recipes

--- singularity_recipes/build.sh
+++ singularity_recipes/build.sh
@@ -6,18 +6,14 @@ pushd python
 sudo singularity build --force ../images/python.simg Singularity
 popd
 
-pushd node
-sudo singularity build --force ../images/node.simg Singularity
+pushd reasonml
+sudo singularity build --force ../images/reasonml.simg Singularity
 popd
 
 pushd rust
-sudo singularity build ../images/rust.simg Singularity
-popd
-
-pushd tools
-sudo singularity build ../images/tools.simg Singularity
+sudo singularity build --force ../images/rust.simg Singularity
 popd
 
 pushd vnc
-sudo singularity build ../images/vnc.simg Singularity
+sudo singularity build --force ../images/vnc.simg Singularity
 popd
--- singularity_recipes/python/entry.sh
+++ singularity_recipes/python/entry.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
 
-mkdir -p ${XDG_CONFIG_HOME}/nvim/specs
-cp python_dein.toml ${XDG_CONFIG_HOME}/nvim/specs
-
-mkdir -p ${JUPYTER_CONFIG_DIR}
-mkdir -p ${JUPYTER_CONFIG_DIR}/nbextensions
-/opt/miniconda3/bin/jupyter notebook --generate-config
-git clone https://github.com/lambdalisue/jupyter-vim-binding.git ${XDG_CONFIG_HOME}/jupyter/nbextensions/vim_binding
-/opt/miniconda3/bin/jupyter contrib nbextension install --user
-/opt/miniconda3/bin/jupyter nbextensions_configurator enable --user
-/opt/miniconda3/bin/jupyter-nbextension enable vim_binding/vim_binding
-/opt/miniconda3/bin/jupyter-nbextension enable --py widgetsnbextension --user
-/opt/miniconda3/bin/jupyter-nbextension install rise --py --user
-/opt/miniconda3/bin/jupyter-nbextension enable rise --py --user
-
-nvim -c 'call dein#install()' -c 'q'
-nvim -c 'UpdateRemotePlugins' -c 'q'

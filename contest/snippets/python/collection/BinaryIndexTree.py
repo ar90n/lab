d9237e3eded9bf1f4d4d7abf3edae9f7b5ead8a8
Masahiro Wada <argon.argon.argon@gmail.com>
2019-05-23 20:16:28 +0000

    Add BinaryIndexTree

---
+++ contest/snippets/python/collection/BinaryIndexTree.py
@@ -0,0 +1,27 @@
+class BinaryIndexTree:
+    def __init__(self, n):
+        self._tree = [0] * (n + 1)
+
+    def sum(self, i, j=None):
+        if j is None:
+            f = 0
+            t = i
+        else:
+            f = i
+            t = j
+
+        return self._sum(t) - self._sum(f)
+
+    def _sum(self, i):
+        s = 0
+        i += 1
+        while 0 < i:
+            s += self._tree[i - 1]
+            i -= i & -i
+        return s
+
+    def add(self, i, x):
+        i += 1
+        while i < len(self._tree):
+            self._tree[i - 1] += x
+            i += i & -i

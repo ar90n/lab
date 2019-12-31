195af9de0da3ecd7d843a4b344448f23c04b4023
Masahiro Wada <argon.argon.argon@gmail.com>
2019-12-31 04:56:36 +0000

    Seprate python versoins

--- singularity_recipes/build.sh
+++ singularity_recipes/build.sh
@@ -3,8 +3,19 @@ sudo singularity build --force ../images/common.simg Singularity
 popd
 
 pushd python
-sudo singularity build --force ../images/python.simg Singularity
+sed 's/###PYTHON_VERSION###/3.6/' Singularity.template > Singularity.tmp
+sudo singularity build --force ../images/python36.simg Singularity.tmp
+sed 's/###PYTHON_VERSION###/3.8/' Singularity.template > Singularity.tmp
+sudo singularity build --force ../images/python38.simg Singularity.tmp
+sed 's/###PYTHON_VERSION###/2.7/' Singularity.template > Singularity.tmp
+sudo singularity build --force ../images/python27.simg Singularity.tmp
 popd
+
+pushd node
+sed 's/###NODE_VERSION###/12.13.0/' Singularity.template > Singularity.tmp
+sudo singularity build --force ../images/node12_13.simg Singularity.tmp
+popd
+
 
 pushd aws
 sudo singularity build --force ../images/aws.simg Singularity
@@ -14,14 +25,10 @@ pushd procon
 sudo singularity build --force ../images/procon.simg Singularity
 popd
 
-#pushd reasonml
-#sudo singularity build --force ../images/reasonml.simg Singularity
-#popd
-#
-#pushd rust
-#sudo singularity build --force ../images/rust.simg Singularity
-#popd
-#
-#pushd vnc
-#sudo singularity build --force ../images/vnc.simg Singularity
-#popd
+pushd rust
+sudo singularity build --force ../images/rust.simg Singularity
+popd
+
+pushd vnc
+sudo singularity build --force ../images/vnc.simg Singularity
+popd

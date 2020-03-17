3f9f6ad9ec3be4fa5f706e680b45c63e59ccec88
Masahiro Wada <argon.argon.argon@gmail.com>
2020-03-17 10:42:54 +0000

    Update singularity recipes

--- singularity_recipes/node/entry.sh
+++ singularity_recipes/node/entry.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mkdir -p ${XDG_CONFIG_HOME}/nvim/specs
-cp aws_dein.toml ${XDG_CONFIG_HOME}/nvim/specs
+cp node_dein.toml ${XDG_CONFIG_HOME}/nvim/specs/node_dein.toml
+cp coc-settings.json ${XDG_CONFIG_HOME}/nvim/coc-settings.json
 
 nvim -c 'call dein#install()' -c 'q'
 nvim -c 'UpdateRemotePlugins' -c 'q'

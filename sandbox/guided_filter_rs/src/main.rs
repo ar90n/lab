cd81e411bf5c8af3101481fef3e92bb9ec217a30
Masahiro Wada <argon.argon.argon@gmail.com>
2021-09-26 02:59:34 +0000

    refactor guided filter

--- sandbox/guided_filter_rs/src/main.rs
+++ sandbox/guided_filter_rs/src/main.rs
@@ -1,8 +1,14 @@
-use image::{DynamicImage, GenericImageView, ImageBuffer, Luma, Pixel, Primitive, Rgb};
+use image::{GenericImageView, ImageBuffer, Luma, Pixel, Primitive};
 use num_traits::NumCast;
 
-extern crate image;
-extern crate num_complex;
+fn cast_subpixel<S, T>(pixel: &Luma<S>) -> Luma<T>
+where
+    S: Primitive,
+    T: Primitive,
+{
+    let Luma([data]) = *pixel;
+    Luma([NumCast::from(data).unwrap(); 1])
+}
 
 fn split_channels<I, P, S>(image: &I) -> Vec<ImageBuffer<Luma<S>, Vec<S>>>
 where
@@ -24,7 +30,7 @@ where
     result
 }
 
-fn meand2d<I, S>(image: &I, r: u32) -> ImageBuffer<Luma<f64>, Vec<f64>>
+fn mean2d<I, S>(image: &I, r: u32) -> ImageBuffer<Luma<f64>, Vec<f64>>
 where
     I: GenericImageView<Pixel = Luma<S>>,
     S: Primitive + 'static,
@@ -59,113 +65,79 @@ where
             let mut acc_head_x: f64 = 0.0;
             let mut acc_tail_x: f64 = 0.0;
 
-            while head_x < r {
-                acc_head_x += get_luma_as_f64(image, head_x, y);
-
-                head_x += 1;
-            }
-
-            while mid_x <= r {
-                acc_head_x += get_luma_as_f64(image, head_x, y);
-
-                let pixel = Luma([calc_mean(acc_head_x, acc_tail_x, r); 1]);
-                result.put_pixel(y, mid_x, pixel);
-
-                head_x += 1;
-                mid_x += 1;
-            }
-
-            while head_x < width {
-                acc_head_x += get_luma_as_f64(image, head_x, y);
-                acc_tail_x += get_luma_as_f64(image, tail_x, y);
-
-                let pixel = Luma([calc_mean(acc_head_x, acc_tail_x, r); 1]);
-                result.put_pixel(y, mid_x, pixel);
-
-                head_x += 1;
-                mid_x += 1;
-                tail_x += 1;
-            }
-
             while mid_x < width {
-                acc_tail_x += get_luma_as_f64(image, tail_x, y);
-
-                let pixel = Luma([calc_mean(acc_head_x, acc_tail_x, r); 1]);
-                result.put_pixel(y, mid_x, pixel);
-
-                mid_x += 1;
-                tail_x += 1;
+                if head_x < width {
+                    acc_head_x += get_luma_as_f64(image, head_x, y);
+                    head_x += 1;
+                }
+                if r < mid_x {
+                    acc_tail_x += get_luma_as_f64(image, tail_x, y);
+                    tail_x += 1;
+                }
+                if r <= head_x {
+                    let pixel = Luma([calc_mean(acc_head_x, acc_tail_x, r); 1]);
+                    result.put_pixel(y, mid_x, pixel);
+                    mid_x += 1;
+                }
             }
         }
 
         result
     }
 
-    let tr_tmp = meand2d_tr(image, r);
-    meand2d_tr(&tr_tmp, r)
+    meand2d_tr(&meand2d_tr(image, r), r)
+}
+
+fn cals_eps(eps: f64) -> f64 {
+    let eps = 255.0 * eps;
+    eps * eps
 }
 
 fn main() {
-    let img = image::open("./cat.jpg").unwrap();
-    let imgbuf = split_channels(&img);
+    let r = 18;
+    let eps = cals_eps(0.1);
+    let img = image::open("./cat.jpg").unwrap().to_luma8();
 
-    let r = 4;
-    let i = &imgbuf[0];
-    let mean_i = meand2d(i, r);
+    let i = &img;
+    let mean_i = mean2d(i, r);
 
     let (w, h) = i.dimensions();
     let ii = ImageBuffer::from_fn(w, h, |x, y| {
-        let Luma(data) = i.get_pixel(x, y);
-        let v = data[0] as f64;
-        Luma([v * v; 1])
+        cast_subpixel(i.get_pixel(x, y)).map(|v: f64| v * v)
     });
-    let corr_i = meand2d(&ii, r);
+    let corr_i = mean2d(&ii, r);
 
     let var_i = ImageBuffer::from_fn(w, h, |x, y| {
-        let Luma(mean_v) = mean_i.get_pixel(x, y);
-        let Luma(corr_v) = corr_i.get_pixel(x, y);
-        let v = corr_v[0] - mean_v[0] * mean_v[0];
-        Luma([v; 1])
+        mean_i
+            .get_pixel(x, y)
+            .map2(corr_i.get_pixel(x, y), |m, c| c - m * m)
     });
 
-    let eps = 255.0 * 0.8;
-    let eps = eps * eps;
     let a = ImageBuffer::from_fn(w, h, |x, y| {
-        let Luma(var_v) = var_i.get_pixel(x, y);
-        let v = var_v[0] / (var_v[0] + eps);
-        Luma([v; 1])
+        var_i.get_pixel(x, y).map_without_alpha(|v| v / (v + eps))
     });
 
     let b = ImageBuffer::from_fn(w, h, |x, y| {
-        let Luma(mean_v) = mean_i.get_pixel(x, y);
-        let Luma(a_v) = a.get_pixel(x, y);
-        let v = (1.0 - a_v[0]) * mean_v[0];
-        Luma([v; 1])
+        mean_i
+            .get_pixel(x, y)
+            .map2(a.get_pixel(x, y), |m, a| (1.0 - a) * m)
     });
 
-    let mean_a = meand2d(&a, r);
-    let mean_b = meand2d(&b, r);
+    let mean_a = mean2d(&a, r);
+    let mean_b = mean2d(&b, r);
 
     let q = ImageBuffer::from_fn(w, h, |x, y| {
-        let Luma(mean_a_v) = mean_a.get_pixel(x, y);
-        let Luma(i_v) = i.get_pixel(x, y);
-        let Luma(mean_b_v) = mean_b.get_pixel(x, y);
-        let v = mean_a_v[0] * i_v[0] as f64 + mean_b_v[0];
-        Luma([v; 1])
-    });
+        let i_v = {
+            let Luma([data]) = *i.get_pixel(x, y);
+            data as f64
+        };
 
-    let vv = ImageBuffer::from_fn(w, h, |x, y| {
-        let Luma(data) = q.get_pixel(x, y);
-        Luma([data[0] as u8; 1])
+        mean_a
+            .get_pixel(x, y)
+            .map2(mean_b.get_pixel(x, y), |a, b| a * i_v + b)
+    });
+    let vv = ImageBuffer::from_fn(w, h, |x, y| -> Luma<u8> {
+        cast_subpixel(q.get_pixel(x, y))
     });
     vv.save(format!("tmp_{}.png", eps)).unwrap();
-    //for (i, v) in imgbuf.iter().enumerate() {
-    //    let vv = meand2d(v, 4);
-    //    let (w, h) = vv.dimensions();
-    //    let vv = ImageBuffer::from_fn(w, h, |x, y| {
-    //        let Luma(data) = vv.get_pixel(x, y);
-    //        Luma([data[0] as u8; 1])
-    //    });
-    //    vv.save(format!("tmp_{}.png", i)).unwrap();
-    //}
 }

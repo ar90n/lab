7c87be11844172f99acec08355879fd1971b49a7
Masahiro Wada <argon.argon.argon@gmail.com>
2023-07-18 00:46:24 +0000

    add runa_cast.py

---
+++ sandbox/build123d_practice/runa_cast.py
@@ -0,0 +1,62 @@
+# %%
+import pickle
+from pathlib import Path
+
+# %%
+import numpy as np
+from build123d import *
+from ocp_vscode import show
+
+# %%
+runa_points_path = Path.cwd() / "runa_points.pickle"
+data = pickle.loads(runa_points_path.read_bytes())
+inner = np.array(data["inner"])
+inner = inner - np.mean(inner, axis=0)
+outer = np.array(data["outer"])
+outer = outer - np.mean(outer, axis=0)
+# %%
+mm_over_pix = 55 / (np.max(inner[:,0]) - np.min(inner[:,0]))
+mm_data_inner = mm_over_pix * inner
+mm_data_outer = mm_over_pix * outer
+
+# %%
+inner_pts = [(float(x), float(y)) for x, y in mm_data_inner]
+outer_pts = [(float(x), float(y)) for x, y in mm_data_outer]
+# %%
+with BuildPart() as part:
+#    with BuildSketch(Plane.XY) as outer_sk:
+#        with BuildLine() as outer_ln:
+#            l1 = Polyline(*outer_pts)
+#            l2 = Line(l1 @ 1, l1 @ 0)
+#        make_face()
+#    extrude(amount=5)
+    with BuildSketch(Plane.XY) as inner_sk:
+        with BuildLine() as inner_ln:
+            l1 = Polyline(*inner_pts)
+            l2 = Line(l1 @ 1, l1 @ 0)
+        make_face()
+    #extrude(amount=5, mode=Mode.SUBTRACT)
+    extrude(amount=5)
+
+#    topf = part.faces().sort_by(Axis.Z)[-1]
+#    offset(amount=-1, openings=topf)
+
+# %%
+part.part
+# %%
+pts
+# %%
+# %%
+# %%
+# %%
+# %%
+import matplotlib.pyplot as plt
+g = np.zeros((600, 600))
+n = 0.85
+nn = int(outer.shape[0] * n)
+g[outer[:nn,0],outer[:nn, 1]] = 100
+plt.imshow(g)
+# %%
+# %%
+len(outer)
+# %%

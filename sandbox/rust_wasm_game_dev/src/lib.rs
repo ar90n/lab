53aeee62ada5e542c42a39ea4eb6bae963694d83
Masahiro Wada <argon.argon.argon@gmail.com>
2023-07-20 06:14:20 +0000

    update

--- sandbox/rust_wasm_game_dev/src/lib.rs
+++ sandbox/rust_wasm_game_dev/src/lib.rs
@@ -1,7 +1,11 @@
+use futures::io::ReadVectored;
+use rand::*;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-use rand::*;
-
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -11,45 +15,30 @@ use rand::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], color: (u8, u8, u8)) {
-    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-
-    let [top, left, right] = points;
-    context.move_to(top.0, top.1);
-    context.begin_path();
-    context.line_to(left.0, left.1);
-    context.line_to(right.0, right.1);
-    context.line_to(top.0, top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
+#[derive(Deserialize)]
+struct Sheet {
+    frames: HashMap<String, Cell>,
 }
 
-fn midpoints(piont_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-    ((piont_1.0 + point_2.0) / 2.0, (piont_1.1 + point_2.1) / 2.0)
+#[derive(Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
 }
 
-fn sierpinski(context: &web_sys::CanvasRenderingContext2d , points: [(f64, f64); 3], color: (u8, u8, u8), depth: u8) {
-    draw_triangle(context, points, color);
+#[derive(Deserialize)]
+struct Cell {
+    frame: Rect,
+}
 
-    let mut rng = thread_rng();
-    let next_color = (
-        rng.gen_range(0..255),
-        rng.gen_range(0..255),
-        rng.gen_range(0..255),
-    );
+async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
+    let window = web_sys::window().unwrap();
+    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
+    let resp: web_sys::Response = resp_value.dyn_into()?;
 
-    let depth = depth - 1;
-    let [top, left, right] = points;
-    if 0 < depth  {
-        let left_middle = midpoints(top, left);
-        let right_middle = midpoints(top, right);
-        let bottom_middle = midpoints(left, right);
-        sierpinski(context, [top, left_middle, right_middle], next_color, depth);
-        sierpinski(context, [left_middle, left, bottom_middle], next_color, depth);
-        sierpinski(context, [right_middle, bottom_middle, right], next_color, depth)
-    }
+    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
 }
 
 // This is like the `main` function, except for JavaScript.
@@ -71,7 +60,59 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], (0, 255, 0), 5);
+    wasm_bindgen_futures::spawn_local(async move {
+        let json = fetch_json("rhb.json")
+            .await
+            .expect("Could not fetch rhb.json");
+        let sheet: Sheet = json
+            .into_serde()
+            .expect("Could not convert rhb.json into a Sheet structure");
+
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+
+        let image = web_sys::HtmlImageElement::new().unwrap();
+        image.set_src("rhb.png");
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                success_tx.send(Ok(()));
+            }
+        });
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                error_tx.send(Err(err));
+            }
+        });
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+        callback.forget();
+        success_rx.await;
+
+        let mut frame = -1;
+        let interval_callback = Closure::wrap(Box::new(move || {
+            context.clear_rect(0.0, 0.0, 600.0, 600.0);
+            frame = (frame + 1) % 8;
+            let frame_name = format!("Run ({}).png", frame + 1);
+            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
+            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                sprite.frame.x.into(),
+                sprite.frame.y.into(),
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+                300.0,
+                300.0,
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+            );
+        }) as Box<dyn FnMut()>);
+        window.set_interval_with_callback_and_timeout_and_arguments_0(
+            interval_callback.as_ref().unchecked_ref(),
+            50
+        );
+        interval_callback.forget();
+    });
 
     Ok(())
 }
